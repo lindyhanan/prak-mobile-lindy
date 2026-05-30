a2badbe188843b4d15d1f9fb54e5d2483c803a7f
lindyhanan <lindy24si@mahasiswa.pcr.ac.id>
2026-05-30 11:14:34 +0700

    P10. Tab Layout

---
+++ app/src/main/java/com/example/orangcantkikapps/Home/pertemuan_10/TabAFragment.kt
@@ -0,0 +1,28 @@
+package com.example.orangcantkikapps.Home.pertemuan_10
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.orangcantkikapps.databinding.FragmentTabABinding
+
+class TabAFragment : Fragment() {
+    private var _binding: FragmentTabABinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentTabABinding.inflate(inflater, container, false)
+        return binding.root
+    }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
+//        (requireActivity() as AppCompatActivity).supportActionBar?.apply {
+//            title = "Message"
+//        }
+//    }
+}
